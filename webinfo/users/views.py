0f6bbf27fa28d82db9f39316e2c9efa3dc314e72
WesleyWanyama <83776940+WesleyWanyama@users.noreply.github.com>
2023-09-12 22:44:47 +0300

    fff

--- webinfo/users/views.py
+++ webinfo/users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Wesley</h1>")
+    return render(request, 'users/home.html')
