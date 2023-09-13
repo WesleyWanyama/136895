86dbfe7041d6b77b40f1d42a03940f750731dee1
Wesley Wanyama <83776940+WesleyWanyama@users.noreply.github.com>
2023-09-13 17:48:27 +0300

    Registration

--- webinfo/users/urls.py
+++ webinfo/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home') #we defined home function in views.py
+    path('', views.home, name='home'),#we defined home function in views.py
+    path('register/', views.register, name='register')
 ]
--- webinfo/users/views.py
+++ webinfo/users/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
-    return HttpResponse("<h1>Wesy</h1>")
+    return render(request, 'users/home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+
+
+    return render(request, 'users/register.html', {'form':form})
 
 
