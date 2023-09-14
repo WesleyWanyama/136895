2bf1fba3a16e62ba158388d87acec965d2921ea5
Wesley Wanyama <83776940+WesleyWanyama@users.noreply.github.com>
2023-09-15 02:20:36 +0300

    otp

--- webinfo/users/views.py
+++ webinfo/users/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from . forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
+from .utils import send_otp 
+from datetime import datetime
+import pyotp
 
 def home(request):
     return render(request, 'users/home.html')
@@ -26,4 +29,17 @@ def profile(request):
     return render(request, 'users/profile.html')
 
 def otp_view(request):
+    error_message = None
+    if request.method == 'POST':
+        otp = request.POST['otp']
+        username = request.session['username']
+
+        otp_secret_key = request.session['otp_secret_key']
+        otp_valid_date = request.session['otp_valid_date']
+
+        if otp_secret_key and otp_valid_date is not None:
+            valid_date = datetime.fromisoformat(otp_valid_date)
+
+            if valid_date > datetime.now():
+                totp = pyotp
     return render(request, 'otp.html', {})
