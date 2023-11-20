4f79ac5470814bd0bbd3be68a1c675ef6e4095d2
Wesley Wanyama <83776940+WesleyWanyama@users.noreply.github.com>
2023-11-20 12:16:39 +0300

    changes

--- webinfo/users/models.py
+++ webinfo/users/models.py
@@ -42,6 +42,9 @@ class UploadedFile(models.Model):
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.file.name
+
 class DownloadDataModel(models.Model):
     # Define your fields here, e.g., name, content, etc.
     name = models.CharField(max_length=255)
